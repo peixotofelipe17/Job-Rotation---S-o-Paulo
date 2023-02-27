b6de98b4c88457954043371316a4cd273a842499
peixotofelipe17 <115385840+peixotofelipe17@users.noreply.github.com>
2023-02-27 16:38:06 -0300

    Update faturamento_mensal.py

--- faturamento_mensal.py
+++ faturamento_mensal.py
@@ -1,8 +1,43 @@
 import json
 
-with open("dados.json") as meu_json:
-    dados = json.load(meu_json)
 
-print(dados)
+meu_json = open("C:/Users/Felipe/Documents/Estudos autonomos/target/dados.json",'r')
+dados = json.load(meu_json)
 
-#def menor_faturamento():
+def menor_faturamento():
+    #No enunciado não diz se é para ignorar os dias sem faturamento no calculo do menor valor, apenas no cálculo da média
+    primeiro_elemento = dados[0]
+    menor_valor = primeiro_elemento['valor']
+    for elemento in dados[1:]:
+        if elemento['valor'] < menor_valor:
+            menor_valor = elemento['valor']
+    return menor_valor
+
+def maior_faturamento():
+    maior_valor = 0
+    for elemento in dados:
+        if elemento['valor'] > maior_valor:
+            maior_valor = elemento['valor']
+    return maior_valor
+
+def dias_acima_media(media):
+    nmr_de_dias = 0
+    for elemento in dados:
+        if elemento['valor'] > media:
+            nmr_de_dias+=1
+    return nmr_de_dias
+
+
+
+total_de_faturamento = 0
+dias_faturados = 0
+for elemento in dados:
+    if elemento['valor'] > 0:
+        total_de_faturamento = total_de_faturamento + elemento['valor']
+        dias_faturados+=1
+media = total_de_faturamento / dias_faturados
+
+print(media)
+print(menor_faturamento())
+print(maior_faturamento())
+print(dias_acima_media(media))
